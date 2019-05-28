571b64157075009b465a1ab2d26971e1a4957ed5
trink <treelink@126.com>
2019-05-28 23:02:22 +0800

    variable

--- test.go
+++ test.go
@@ -2,7 +2,26 @@ package main
 
 import "fmt"
 
+var x, y int
+var (
+	a int
+	b bool
+)
+
+var c, d = 1, 2
+var e, f = 123, "hello"
+
 func main() {
+	dataType()
+	variable()
+}
+
+func variable() {
+	g, h := 123, "hello"
+	fmt.Println(x, y, a, b, c, d, e, f, g, h)
+}
+
+func dataType() {
 	var f float32 = 3.4
 	var i, i64, d, cx = 10, 64, 3.099128981278123123, 12 + 3i
 	var s, ptr = "qwe", uintptr(i)
