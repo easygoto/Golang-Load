12606838639227dbd7f2dc632ee2bb8f4ea5846a
trink <treelink@126.com>
2019-05-28 23:36:03 +0800

    update

--- test.go
+++ test.go
@@ -2,30 +2,15 @@ package main
 
 import "fmt"
 
-var x, y int
-var (
-	a int
-	b bool
-)
-
-var c, d = 1, 2
-var e, f = 123, "hello"
+var f float32 = 3.4
+var i, i64, d, cx = 10, 64, 3.099128981278123123, 12 + 3i
 
 func main() {
 	dataType()
 	//variable()
 }
 
-func variable() {
-	g, h := 123, "hello"
-	fmt.Println(&x)
-	x = 100
-	fmt.Println(x, &x, y, a, b, c, d, e, f, g, h)
-}
-
 func dataType() {
-	var f float32 = 3.4
-	var i, i64, d, cx = 10, 64, 3.099128981278123123, 12 + 3i
 	var s, ptr = "qwe", uintptr(i)
 	fmt.Println(&i)
 	i = 21
