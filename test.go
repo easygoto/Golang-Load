dc7977652f8bd0490940743df3b5c475a5b63364
trink <treelink@126.com>
2019-05-28 23:32:18 +0800

    update

--- test.go
+++ test.go
@@ -13,25 +13,27 @@ var e, f = 123, "hello"
 
 func main() {
 	dataType()
-	variable()
+	//variable()
 }
 
 func variable() {
 	g, h := 123, "hello"
-	fmt.Println(x, y, a, b, c, d, e, f, g, h)
+	fmt.Println(&x)
+	x = 100
+	fmt.Println(x, &x, y, a, b, c, d, e, f, g, h)
 }
 
 func dataType() {
 	var f float32 = 3.4
 	var i, i64, d, cx = 10, 64, 3.099128981278123123, 12 + 3i
 	var s, ptr = "qwe", uintptr(i)
-
+	fmt.Println(&i)
 	i = 21
-
+	fmt.Println(&i)
 	fmt.Println(i64 + i)
 	fmt.Println(f)
 	fmt.Println(d * float64(f))
 	fmt.Println(cx * cx)
 	fmt.Println(s + "12")
-	fmt.Println(ptr)
+	fmt.Println(&ptr)
 }
