f7c32f875de397cb93c3b85f986e2680140855bc
trink <treelink@126.com>
2019-06-08 18:43:18 +0800

    fibonacci

--- test.go
+++ test.go
@@ -31,7 +31,15 @@ func main() {
 	//_struct()
 	//slice()
 	//xRange()
-	mapDemo()
+	//mapDemo()
+	fmt.Println(fibonacci(15))
+}
+
+func fibonacci(n int) int {
+	if n < 2 {
+		return n
+	}
+	return fibonacci(n-2) + fibonacci(n-1)
 }
 
 func mapDemo() {
