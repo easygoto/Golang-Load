6c2942a12d830055ec74b25c1295e329aeb23ad0
trink <treelink@126.com>
2019-06-13 22:24:13 +0800

    tab -> space

--- main.go
+++ main.go
@@ -4,15 +4,15 @@ import "fmt"
 import "./HashMap"
 
 func main() {
-	testHashMap()
+    testHashMap()
 }
 
 func testHashMap() {
-	HashMap.GetInstance()
-	HashMap.Put("a", "a_put")
-	HashMap.Put("b", "b_put")
-	fmt.Println(HashMap.Get("a"))
-	fmt.Println(HashMap.Get("b"))
-	HashMap.Put("p", "p_put")
-	fmt.Println(HashMap.Get("p"))
+    HashMap.GetInstance()
+    HashMap.Put("a", "a_put")
+    HashMap.Put("b", "b_put")
+    fmt.Println(HashMap.Get("a"))
+    fmt.Println(HashMap.Get("b"))
+    HashMap.Put("p", "p_put")
+    fmt.Println(HashMap.Get("p"))
 }
